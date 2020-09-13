e15fe5cc7f2899fb663919d721d0bf0efecaf8c7
Pelat-1 <nicolo.rogai@outlook.com>
2020-09-13 15:40:20 +0200

    Created alert-manager component, it will decide when and how show alerts

---
+++ src/app/alert-manager/alert-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-manager',
+  templateUrl: './alert-manager.component.html',
+  styleUrls: ['./alert-manager.component.css']
+})
+export class AlertManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlertManagerComponent } from './alert-manager/alert-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertManagerComponent
   ],
   imports: [
     BrowserModule
