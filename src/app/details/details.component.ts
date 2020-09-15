8930c4d82b28c4085739d9ff3190fcac4fc03ad4
Pelat-1 <nicolo.rogai@outlook.com>
2020-09-16 00:28:56 +0200

    Toggle implemented

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  displayParagraph = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleDisplay(): void {
+    this.displayParagraph = !this.displayParagraph;
+  }
 }
